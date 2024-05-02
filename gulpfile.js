941c54b8fe3ccef6c361ff47a5f57a4065e281aa
Molly <117697419+learningcoding2022@users.noreply.github.com>
2024-05-02 10:35:18 -0400

    project structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('index.scss')
+  /* need to change this file below*/
+  return src('default-sass/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  /**need to change this file below */
+  watch(['default-sass/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
