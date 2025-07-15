15b2fd3555d5ee9aa5bc354b1b3d6ce3d390ad0e
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-07-15 16:51:02 -0400

    lesson 20

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
   /* need to change this file below*/
   return src('default-sass/**/*.scss') //**means any sub folder of scss will be watched, but not compiled since the folders have _ */
-  .pipe(sass())
-    .pipe(dest('css'))
+      .pipe(sass({ outputStyle: 'compressed' }))
+      .pipe(purgecss({ content: ['*.html'] }))
+      .pipe(dest('css'))
 }
+  
+
 
 function watchTask() {
   /**need to change this file below */
-  watch(['default-sass/**/*.scss'], buildStyles)
+  watch(['default-sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
