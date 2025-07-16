9f72c59aca5e5bc07864401f8e02e24298b35278
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-07-16 17:30:22 -0400

    added sass folder and scss file

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,18 @@
 const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
-const purgecss = require('gulp-purgecss');
+const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
   /* need to change this file below*/
-  return src('default-sass/**/*.scss') //**means any sub folder of scss will be watched, but not compiled since the folders have _ */
-      .pipe(sass({ outputStyle: 'compressed' }))
-      .pipe(purgecss({ content: ['*.html'] }))
-      .pipe(dest('css'))
+  return src('sass/**/*.scss') //**means any sub folder of scss will be watched, but not compiled since the folders have _ */
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(purgecss({ content: ['*.html'] }))
+    .pipe(dest('css'))
 }
-  
-
 
 function watchTask() {
   /**need to change this file below */
-  watch(['default-sass/**/*.scss', '*.html'], buildStyles)
+  watch(['sass/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
